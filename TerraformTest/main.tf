678c5912a74501168980a9c7ac7437a4d5d38a0b
Your Name <you@example.com>
2025-07-31 13:01:42 +0530

    updates

--- TerraformTest/main.tf
+++ TerraformTest/main.tf
@@ -9,11 +9,15 @@ terraform {
     required_version = ">= 0.14.9"
 }
 
+#This configures the Azure RM provider to use the specified subscription.
 provider "azurerm" {
   subscription_id = "xxxx-xxxx-xxx-xxx-xxx"
   features {}
 }
 
+#This fetches an existing resource group named Priyanka_RSG_test
+#'data' - It will make the existing resource available to other Terraform resources.
+#Eg. we can refer to its location using data.azurerm_resource_group.rsg.location
 data "azurerm_resource_group" "rsg" {
     name="Priyanka_RSG_test"
 }
